4ab34615710102da108b4790896ad2067a0d5a5b
yanghu <huyang@mail.bistu.edu.cn>
2017-07-06 20:45:03 +0800

    Initial Commit

---
+++ testClangFormat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testClangFormat
+//
+//  Created by young on 2017/7/6.
+//  Copyright © 2017年 young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testClangFormat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testClangFormat
+//
+//  Created by young on 2017/7/6.
+//  Copyright © 2017年 young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
