2851f893d2fdf95af825ac9bf761739db55bd7b5
yanghu <huyang@mail.bistu.edu.cn>
2017-07-09 01:22:26 +0800

    no message

--- testClangFormat/AppDelegate.m
+++ testClangFormat/AppDelegate.m
@@ -8,20 +8,9 @@
 
 #import "AppDelegate.h"
 
-
-
-
-
-     @interface AppDelegate ()
+@interface AppDelegate ()
 
 @end
-
-
-
-
-
-
-
 
 @implementation AppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
