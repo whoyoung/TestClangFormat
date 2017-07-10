aa6356967193b4157ef795425d28f6291b4515c6
yanghu <huyang@mail.bistu.edu.cn>
2017-07-10 22:40:30 +0800

    no message

---
+++ testClangFormat/dir1/file1.m
@@ -0,0 +1,14 @@
+//
+//  file1.m
+//  testClangFormat
+//
+//  Created by young on 2017/7/10.
+//  Copyright © 2017年 young. All rights reserved.
+//
+
+#import "file1.h"
+
+
+@implementation file1
+
+@end
